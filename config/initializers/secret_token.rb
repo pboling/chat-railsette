091f53be20780d7f92aa672278388c750719bb57
Peter Boling <peter.boling@gmail.com>
2012-08-08 10:49:47 -0400

    Blank Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChatRailsette::Application.config.secret_token = '89e167eaefc87d81e12b48003d5d0f0f2a01652ed78ffeb286eae9d912b42a80d8ef0927ef52456cd51ca769c79475f0151f50ea971339e636d162c7a3020202'
