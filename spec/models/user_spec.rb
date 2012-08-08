fe1be3644f151108a540db69bff78b452d207433
Peter Boling <peter.boling@gmail.com>
2012-08-08 11:07:40 -0400

    User Spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be saved successfully" do
+    User.create.should be_persisted
+  end
 end
